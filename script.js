b1c60419d978103962881c7b62a3028783f8b6a6
rajveer doriya <rajveerdoriyaofficial@gmail.com>
2024-08-11 23:25:46 +0530

    run program

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const accountId=746;
 let  accountEmail="hello@gmail.com";
 var accountPassword=765444343;
+let accountState;
+
+
+console.log(accountState);
+
 console.log(accountId);
 accountCity="jaipur";
 console.log(accountEmail);
@@ -8,4 +13,5 @@ console.log(accountEmail);
 accountEmail="hii@gmail.com";
 accountPassword=87453875;
 
-console.table([accountId,accountEmail,accountCity,accountPassword]);
+console.table([accountId,accountEmail,accountState,accountCity,accountPassword]);
+/*   coomment  */
